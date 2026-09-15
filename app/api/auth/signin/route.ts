e3cec31729de51f98acd62bac5c61f4a56b6e370
adityaptm <putraadityapratama236@gmail.com>
2026-09-15 12:20:23 +0700

    FIX LANSENA HARI SELASA V3

--- app/api/auth/signin/route.ts
+++ app/api/auth/signin/route.ts
@@ -16,7 +16,6 @@ async function logLogin(
     const ip =
       req.headers.get('x-forwarded-for')?.split(',')[0]?.trim() ||
       req.headers.get('x-real-ip') ||
-      req.ip ||
       '127.0.0.1';
     const userAgent = req.headers.get('user-agent') || '-';
 
